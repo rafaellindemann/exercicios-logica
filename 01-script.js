0326c9b25df310ae4986122eb88193c3f89e812f
Rafael Lindemann Duarte <69912539+rafaellindemann@users.noreply.github.com>
2025-05-28 17:07:07 -0300

    Adicionados exercícios de JS

---
+++ 01-script.js
@@ -0,0 +1,12 @@
+
+// sq2 - ex 9
+// Mensagem Secreta: Peça ao usuário para digitar uma senha e, se a senha for "abracadabra", exiba uma mensagem secreta.
+
+let senha 
+
+senha = prompt("Digite a senha")
+if(senha == "abracadabra"){
+    alert("Senha está correta, bem vindo ao mundo secreto")
+}else{
+    alert("Corre que vou chamar a polícia")
+}
---
+++ 02-script.js
@@ -0,0 +1,4 @@
+// sq2 - ex 13
+// Verificando Votação: Peça ao usuário para inserir sua idade e verifique se ele é elegível para votar (idade mínima de 16 anos).
+
+
